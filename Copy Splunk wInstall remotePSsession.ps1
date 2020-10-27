d9d49690abbd02fbf831c6c06865f9087d09ddf6
Kev224h <63749541+Kev224h@users.noreply.github.com>
2020-10-27 16:18:39 +0000

    Add files via upload

---
+++ Copy Splunk wInstall remotePSsession.ps1
@@ -0,0 +1,32 @@
+﻿foreach($line in Get-content .\servers.txt) {
+	if ((test-path "\\$line\c$\install\") -eq $true){
+        Write-Host "Copying splunkforwarder to server!" -ForegroundColor yellow
+		
+    }	
+	Write-Host "Copying splunkforwarder to $line"
+	robocopy c:\Install\splunkforwarder-7.3.5-86fd62efc3d7-x64-release \\$line\c$\Install\splunkforwarder-7.3.5-86fd62efc3d7-x64-release /e /z /MT:32
+    Write-Host "Copy Complete" -ForegroundColor green
+    #Write-Host "Installing splunkforwarder-735 to $line" -ForegroundColor yellow	
+    #msiexec /i \\$line\c:\Install\splunkforwarder-7.3.5-86fd62efc3d7-x64-release\splunkforwarder-7.3.5-86fd62efc3d7-x64-release.msi DEPLOYMENT_SERVER="mon-us2p-dm-1a.adminsys.mrll.com:8089" AGREETOLICENSE=Yes /quiet
+
+	
+        
+	if ((test-path "\\$line\c$\Install\splunkforwarder-7.3.5-86fd62efc3d7-x64-release") -eq $true){
+		Write-Host "Installing splunkforwarder-735 to $line" -ForegroundColor yellow
+		
+	    Enable-PSRemoting
+        #$newtab.Invoke("Enter-PSSession -computername $line")
+        Enter-PSSession -computername $line
+
+		#$newtab.DisplayName = "$line"
+        #$secPasswd=ConvertTo-SecureString "P3anutBr1ttl3" -AsPlainText -Force
+        #$cred=Get-Credential System.Management.Automation.PSCredential -ArgumentList "ADMINSYS\DSLCPDFDevSvc",$secPasswd
+        set-location "\\$line\c$\install\splunkforwarder-7.3.5-86fd62efc3d7-x64-release"
+        Invoke-Command -ComputerName $line -ScriptBlock {
+        #Set-Location "c:\Install\splunkforwarder-7.3.5-86fd62efc3d7-x64-release"
+msiexec /i c:\Install\splunkforwarder-7.3.5-86fd62efc3d7-x64-release\splunkforwarder-7.3.5-86fd62efc3d7-x64-release.msi DEPLOYMENT_SERVER="mon-us2p-dm-1a.adminsys.mrll.com:8089" AGREETOLICENSE=Yes /quiet
+        }
+        Exit-PSSession
+        Write-Host "Install Complete" -ForegroundColor green
+        }
+    }
---
+++ UpdateMySqlrefreshfolder.ps1
@@ -0,0 +1,9 @@
+﻿foreach($line in Get-content .\servers.txt) {
+	if ((test-path "\\$line\f$\mysql_refresh_data") -eq $true){
+        Write-Host "Existing mysql_new folder detected, deleting it!" -ForegroundColor yellow
+		Remove-Item -path "\\$line\f$\mysql_refresh_data" -recurse -force
+    }	
+	Write-Host "Copying mysql_refresh_data folder to $line"
+	robocopy f:\mysql_refresh_data \\$line\f$\mysql_refresh_data /e /z /MT:32
+}
+Write-Host "Copy Complete" -ForegroundColor green
